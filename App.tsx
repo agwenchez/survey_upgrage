ac33745dd1b926d6ecf1215ed811407cebe3408f
wotengs <martin.wangombe@strathmore.edu>
2024-02-28 12:21:59 +0300

    CONVERSION UPGRADE

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
-import { Route, Navigate, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import {
+  Route,
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import Interested from "./src/pages/Survey/InterestedCustomer";
 import UnInterestedSurveyQuestions from "./src/pages/Survey/UnInterestedSurveyQuestions";
 import SurveyLayout from "./src/layouts/SurveyLayout";
 import Congratulations from "./src/pages/Survey/Congratulation";
+import Conversion from "./src/pages/Survey/conversion";
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route>
         <Route path="/survey/intrested" element={<Interested />} />
-        <Route path="/" element={<Navigate to="/survey/interested" replace />} />
-        
+        <Route
+          path="survey/conversion_landing_upgrade"
+          element={<Conversion />}
+        />
+        <Route
+          path="/"
+          element={<Navigate to="/survey/interested" replace />}
+        />
+
         <Route path="/survey" element={<SurveyLayout />}>
           <Route index path="interested" element={<Interested />} />
-          <Route path="uninterested" element={<UnInterestedSurveyQuestions />} />
-          <Route path="congratulations" element={<Congratulations/>} />
+          <Route
+            path="uninterested"
+            element={<UnInterestedSurveyQuestions />}
+          />
+          <Route path="congratulations" element={<Congratulations />} />
+          {/* <Route path="conversion_landing_upgrade" element={<Conversion/>} /> */}
         </Route>
       </Route>
     )
@@ -29,7 +47,4 @@ const App = () => {
 
 export default App;
 
-
-
-
-// const 
+// const
