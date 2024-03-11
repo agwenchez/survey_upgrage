884c183eb352ba33b18ea56cef86bfc92e9ab867
wotengs <martin.wangombe@strathmore.edu>
2024-03-11 16:09:23 +0300

    RESPONSIVE SCREENS ON INDUSTRY SCREENS

--- src/pages/Survey/conversion/index.tsx
+++ src/pages/Survey/conversion/index.tsx
@@ -3,300 +3,348 @@ import styles from "./conversionU.module.scss";
 const Conversion = () => {
   return (
     <>
-      <a
-        href="https://www.imbankgroup.com/ke/"
-        className={styles.top_right_logo}
-      >
-        <img src="/images/logo.png" alt="I&M Bank Logo" />
-      </a>
-      <div className={styles.center_image}>
-        <img src="/images/center.png" alt="WelcomeImage" />
-        <span className={styles.center_text}>Welcome To</span>
-        <br />
-        <p className={styles.center_text}>The Upgrade</p>
-      </div>
-      <p id={styles.midright_text}>
-        <strong>
-          Choose between the Club and PayGo account as
-          <p>desired and enjoy more benefits</p>
-        </strong>
-      </p>
-      <div className={styles.left_selection}>
-        <p>
-          <strong>Club Account</strong>
-        </p>
-        <p>
-          The I&M Club Account is your unrivaled gateway to a world of
-          priveledges that align with your lifestyle requirements, matched with
-          convenient banking services all just one monthly flat fee.
-        </p>
-        <table>
-          <tr>
-            <td>Minimum Balance</td>
-            <td>Nil</td>
-          </tr>
-          <tr>
-            <td>Ledger Fees</td>
-            <td>Nil</td>
-          </tr>
-          <tr>
-            <td>Debit Card</td>
-            <td>Free</td>
-          </tr>
-          <tr>
-            <td>Card Annual/Replacement Fee</td>
-            <td>Nil</td>
-          </tr>
-          <tr>
-            <td>Monthly Fee</td>
-            <td>1000/-</td>
-          </tr>
-          <tr>
-            <td>ATM Withdrawal Fee</td>
-            <td>Free ATM Usage(local and International)</td>
-          </tr>
-          <tr>
-            <td>Pesalink/TT/RGTS</td>
-            <td>Free on Mobile and Internet Banking</td>
-          </tr>
-          <tr>
-            <td>Internal Transfer</td>
-            <td>Free</td>
-          </tr>
-          <tr>
-            <td>Internal Transfer</td>
-            <td>Free</td>
-          </tr>
-        </table>
-        <button id={styles.button} type="button">
-          {" "}
-          Upgrade Now
-        </button>
-      </div>
-      <div className={styles.right_selection}>
-        <p>
-          <strong>PayGo Account</strong>
-        </p>
-        <p>
-          The I&M Club Account is your unrivaled gateway to a world of
-          priveledges that align with your lifestyle requirements, matched with
-          convenient banking services all just one monthly flat fee.
-        </p>
-        <table>
-          <tr>
-            <td>Minimum Balance</td>
-            <td>Nil</td>
-          </tr>
-          <tr>
-            <td>Ledger Fees</td>
-            <td>Nil</td>
-          </tr>
-          <tr>
-            <td>Debit Card</td>
-            <td>Free</td>
-          </tr>
-          <tr>
-            <td>Card Annual/Replacement Fee</td>
-            <td>Nil</td>
-          </tr>
-          <tr>
-            <td>Monthly Fee</td>
-            <td>1000/-</td>
-          </tr>
-          <tr>
-            <td>ATM Withdrawal Fee</td>
-            <td>Free ATM Usage(local and International)</td>
-          </tr>
-          <tr>
-            <td>Pesalink/TT/RGTS</td>
-            <td>Free on Mobile and Internet Banking</td>
-          </tr>
-          <tr>
-            <td>Internal Transfer</td>
-            <td>Free</td>
-          </tr>
-          <tr>
-            <td>Internal Transfer</td>
-            <td>Free</td>
-          </tr>
-        </table>
-        <button id={styles.button} type="button">
-          {" "}
-          Upgrade Now
-        </button>
-      </div>
-      <div className={styles.club_benefits}>
-        <p>
-          <strong>Other Club Benefits</strong>
-        </p>
-        <table>
-          <tr>
-            <td>Minimum Balance</td>
-            <td>Nil</td>
-          </tr>
-          <tr>
-            <td>Ledger Fees</td>
-            <td>Nil</td>
-          </tr>
-          <tr>
-            <td>Debit Card</td>
-            <td>Free</td>
-          </tr>
-          <tr>
-            <td>Card Annual/Replacement Fee</td>
-            <td>Nil</td>
-          </tr>
-          <tr>
-            <td>Card Annual/Replacement Fee</td>
-            <td>Nil</td>
-          </tr>
-          <tr>
-            <td>Card Annual/Replacement Fee</td>
-            <td>Nil</td>
-          </tr>
-          <tr>
-            <td>Card Annual/Replacement Fee</td>
-            <td>Nil</td>
-          </tr>
-        </table>
-        <hr />
-      </div>
-      <div className={styles.paygo_benefits}>
-        <p>
-          <strong>Other PayGo Benefits</strong>
-        </p>
-        <table>
-          <tr>
-            <td>Minimum Balance</td>
-            <td>Nil</td>
-          </tr>
-          <tr>
-            <td>Ledger Fees</td>
-            <td>Nil</td>
-          </tr>
-          <tr>
-            <td>Debit Card</td>
-            <td>Free</td>
-          </tr>
-          <tr>
-            <td>Card Annual/Replacement Fee</td>
-            <td>Nil</td>
-          </tr>
-          <tr>
-            <td>Card Annual/Replacement Fee</td>
-            <td>Nil</td>
-          </tr>
-          <tr>
-            <td>Card Annual/Replacement Fee</td>
-            <td>Nil</td>
-          </tr>
-          <tr>
-            <td>Card Annual/Replacement Fee</td>
-            <td>Nil</td>
-          </tr>
-        </table>
-        <hr />
-      </div>
-      <div className={styles.left_bottom_table}>
-        <table>
-          <p>
-            <strong>More Club Benefits</strong>
-          </p>
-          <tr>
-            <td>
-              <img src="/images/card.png" alt="CardImage" />
-              Free Debit Card
-            </td>
-            <td>
-              <img src="/images/plane.png" alt="AmrefPlaneImage" />
-              Free AMREF Maisha Bronze Cover
-            </td>
-            <td>
-              {" "}
-              <img src="/images/networking.png" alt="NetworkingImage" />
-              Access To Professional Networking Forums
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img src="/images/instructions.png" alt="InstructionImage" />
-              Free Standing Instruction Setup
-            </td>
-            <td>
-              <img src="/images/sms.png" alt="SMSNotificationImage" />
-              Free SMS Notification
-            </td>
-            <td>
-              <img src="/images/rewards.png" alt="RewardsImage" />
-              Rewards And Personalised offers
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img src="/images/atm.png" alt="ATMCardImage" />
-              Free ATM Usage(local and International)
-            </td>
-            <td>
-              <img src="/images/card.png" alt="CardImage" />
-              Free Debit Card
-            </td>
-            <td>
-              <img src="/images/card.png" alt="CardImage" />
-              Free Debit Card
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img src="/images/card.png" alt="CardImage" />
-              Free Debit Card
-            </td>
-            <td>
-              <img src="/images/loan.png" alt="LoanImage" />
-              Personal Unsecured Loan through Digital Channels
-            </td>
-          </tr>
-        </table>
-      </div>
-      <div className={styles.right_bottom_table}>
-        <table>
-          <p>
-            <strong>More PayGo Benefits</strong>
-          </p>
-          <tr>
-            <td>
-              <img src="/images/card.png" alt="CardImage" />
-              Free Debit Card
-            </td>
-            <td>
-              <img src="/images/plane.png" alt="AmrefPlaneImage" />
-              Free AMREF Maisha Bronze Cover
-            </td>
-            <td>
-              {" "}
-              <img src="/images/networking.png" alt="NetworkingImage" />
-              Access To Professional Networking Forums
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img src="/images/instructions.png" alt="InstructionImage" />
-              Free Standing Instruction Setup
-            </td>
-          </tr>
-        </table>
-      </div>
-      <p className={styles.terms}>
-        <strong>
-          @2021 I&M Bank Limited, All rights reserved.
-          <br />
-          <a
-            target="_blank"
-            href="https://www.imbankgroup.com/ke/terms-and-conditions/"
-          >
-            Terms & Conditions.
+      <div className={styles.main_container}>
+        <div className={styles.top_right_logo}>
+          <a target="_blank" href="https://www.imbankgroup.com/ke/">
+            <img src="/images/logo.png" alt="I&M Bank Logo" />
           </a>
-        </strong>
-      </p>
+        </div>
+        <div className={styles.top_text}>
+          <span>
+            Choose between the Club and PayGo account as
+            desired and enjoy more benefits
+          </span>
+        </div>
+        <div className={styles.swoosh}>
+          <img src="/images/center.png" alt="WelcomeImage" />
+          <span>
+            Welcome To <br />
+            The Upgrade
+          </span>
+        </div>
+        <div className={styles.left_selection}>
+          <span>
+            <strong>Club Account</strong>
+          </span>
+          <p>
+            The I&M Club Account is your unrivaled gateway to a world of
+            priveledges that align with your lifestyle requirements, matched
+            with convenient banking services all just one monthly flat fee.
+          </p>
+
+          <table>
+            <tr>
+              <td>Minimum Balance</td>
+              <td>
+                <em>Nil</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Ledger Fees</td>
+              <td>
+                <em>Nil</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Debit Card</td>
+              <td>
+                <em>Free</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Card Annual/Replacement Fee</td>
+              <td>
+                <em>Nil</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Monthly Fee</td>
+              <td>
+                <em>1000/-</em>
+              </td>
+            </tr>
+            <tr>
+              <td>ATM Withdrawal Fee</td>
+              <td>
+                <em>Free ATM Usage(local and International)</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Pesalink/TT/RGTS</td>
+              <td>
+                <em>Free on Mobile and Internet Banking</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Internal Transfer</td>
+              <td>
+                <em>Free</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Internal Transfer</td>
+              <td>
+                <em>Free</em>
+              </td>
+            </tr>
+          </table>
+
+          <button type="button">Upgrade Now</button>
+        </div>
+        <div className={styles.right_selection}>
+          <span>
+            <strong>PayGo Account</strong>
+          </span>
+          <p>
+            The I&M Club Account is your unrivaled gateway to a world of
+            priveledges that align with your lifestyle requirements, matched
+            with convenient banking services all just one monthly flat fee.
+          </p>
+
+          <table>
+            <tr>
+              <td>Minimum Balance</td>
+              <td>
+                <em>Nil</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Ledger Fees</td>
+              <td>
+                <em>Nil</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Debit Card</td>
+              <td>
+                <em>Free</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Card Annual/Replacement Fee</td>
+              <td>
+                <em>Nil</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Monthly Fee</td>
+              <td>
+                <em>1000/-</em>
+              </td>
+            </tr>
+            <tr>
+              <td>ATM Withdrawal Fee</td>
+              <td>
+                <em>Free ATM Usage(local and International)</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Pesalink/TT/RGTS</td>
+              <td>
+                <em>Free on Mobile and Internet Banking</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Internal Transfer</td>
+              <td>
+                <em>Free</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Internal Transfer</td>
+              <td>
+                <em>Free</em>
+              </td>
+            </tr>
+          </table>
+
+          <button type="button">Upgrade Now</button>
+        </div>
+        <div className={styles.club_benefits}>
+          <span>
+            <strong>Other Club Benefits</strong>
+          </span>
+          <table>
+            <tr>
+              <td>Minimum Balance</td>
+              <td>
+                <em>Nil</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Ledger Fees</td>
+              <td>
+                <em>Nil</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Debit Card</td>
+              <td>
+                <em>Free</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Card Annual/Replacement Fee</td>
+              <td>
+                <em>Nil</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Card Annual/Replacement Fee</td>
+              <td>
+                <em>Nil</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Card Annual/Replacement Fee</td>
+              <td>
+                <em>Nil</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Card Annual/Replacement Fee</td>
+              <td>
+                <em>Nil</em>
+              </td>
+            </tr>
+          </table>
+          <hr />
+        </div>
+        <div className={styles.paygo_benefits}>
+          <span>
+            <strong>Other PayGo Benefits</strong>
+          </span>
+          <table>
+            <tr>
+              <td>Minimum Balance</td>
+              <td>
+                <em>Nil</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Ledger Fees</td>
+              <td>
+                <em>Nil</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Debit Card</td>
+              <td>
+                <em>Free</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Card Annual/Replacement Fee</td>
+              <td>
+                <em>Nil</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Card Annual/Replacement Fee</td>
+              <td>
+                <em>Nil</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Card Annual/Replacement Fee</td>
+              <td>
+                <em>Nil</em>
+              </td>
+            </tr>
+            <tr>
+              <td>Card Annual/Replacement Fee</td>
+              <td>
+                <em>Nil</em>
+              </td>
+            </tr>
+          </table>
+          <hr />
+        </div>
+        <span className={styles.span_left}>
+            <strong>More Club Benefits</strong>
+          </span>
+        <div className={styles.grid_container1}>       
+          <div className={styles.grid_item}>
+            <img src="/images/card.png" alt="CardImage" />
+
+            <p>Free Debit Card</p>
+          </div>
+          <div className={styles.grid_item}>
+            <img src="/images/plane.png" alt="AmrefPlaneImage" />
+            <p>Free AMREF Maisha Bronze Cover</p>
+          </div>
+          <div className={styles.grid_item}>
+            <img src="/images/networking.png" alt="NetworkingImage" />
+            <p>Access To Professional Networking Forums</p>
+          </div>
+          <div className={styles.grid_item}>
+            <img src="/images/instructions.png" alt="InstructionImage" />
+            <p>Free Standing Instruction Setup</p>
+          </div>
+          <div className={styles.grid_item}>
+            <img src="/images/sms.png" alt="SMSNotificationImage" />
+            <p>Free SMS Notification</p>
+          </div>
+          <div className={styles.grid_item}>
+            <img src="/images/rewards.png" alt="RewardsImage" />
+            <p>Rewards And Personalised offers</p>
+          </div>
+          <div className={styles.grid_item}>
+            <img src="/images/atm.png" alt="ATMCardImage" />
+            <p>Free ATM Usage(local and International)</p>
+          </div>
+          <div className={styles.grid_item}>
+            <img src="/images/card.png" alt="CardImage" />
+            <p>Free Debit Card</p>
+          </div>
+          <div className={styles.grid_item}>
+            <img src="/images/card.png" alt="CardImage" />
+            <p>Free Debit Card</p>
+          </div>
+          <div className={styles.grid_item}>
+            <img src="/images/card.png" alt="CardImage" />
+            <p>Free Debit Card</p>
+          </div>
+          <div className={styles.grid_item}>
+            <img src="/images/loan.png" alt="LoanImage" />
+            <p>Personal Unsecured Loan through Digital Channels</p>
+          </div>
+        </div>
+        <span className={styles.span_right}>
+            <strong>More PayGo Benefits</strong>
+          </span>
+        <div className={styles.grid_container2}>
+          <div className={styles.grid_item}>
+            <img src="/images/card.png" alt="CardImage" />
+            <p>Free Debit Card</p>
+          </div>
+          <div className={styles.grid_item}>
+            <img src="/images/plane.png" alt="AmrefPlaneImage" />
+            <p>Free AMREF Maisha Bronze Cover</p>
+          </div>
+          <div className={styles.grid_item}>
+            <img src="/images/networking.png" alt="NetworkingImage" />
+            <p>Access To Professional Networking Forums</p>
+          </div>
+          <div className={styles.grid_item}>
+            <img src="/images/instructions.png" alt="InstructionImage" />
+            <p> Free Standing Instruction Setup</p>
+          </div>
+        </div>
+        <footer className={styles.footer}>
+          <span>
+            <strong>
+              @2024 I&M Bank Limited, All rights reserved. <br />
+              <a
+                target="_blank"
+                href="https://www.imbankgroup.com/ke/terms-and-conditions/"
+              >
+                Terms & Condition
+              </a>
+            </strong>
+          </span>
+        </footer>
+      </div>
     </>
   );
 };
