30ad9a1497014a2f031f1fd676b1a1ab5881834b
wotengs <martin.wangombe@strathmore.edu>
2024-03-21 13:46:44 +0300

    ALL PAGES

--- src/pages/Survey/conversion/index.tsx
+++ src/pages/Survey/conversion/index.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import styles from "./conversion.module.scss";
-import { useNavigate } from "react-router-dom";
+import { To, useNavigate } from "react-router-dom";
 
 const Conversion = () => {
   const navigate = useNavigate();
-  const handleClick = (url) => {
+  const handleClick = (url: To) => {
     // Navigate to a next page
     navigate(url);
   };
